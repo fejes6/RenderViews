cd6e81056353e40eafcabf65fbbcad9633f58085
fejes6 <fejesandrej@gmail.com>
2015-06-09 18:05:02 +0200

    Update IllustrativeRenderer.js

--- IllustrativeRenderer.js
+++ IllustrativeRenderer.js
@@ -62,7 +62,7 @@
     //reference to the mesh being currently picked; null if none
     self.picked = null;
     //material wich substitutes the default mesh material when a mesh is picked
-    self.pickedMaterial = new THREE.MeshBasicMaterial({ color: 'red', blending: THREE.NoBlending }});
+    self.pickedMaterial = new THREE.MeshBasicMaterial({ color: 'red', blending: THREE.NoBlending });
     //color: 0x00ff00, transparent: true, blending: THREE.MultiplyBlending
     //At last we add a new update method
     self.updateCalls.push(function () {
