9df651e04a0a9c572647152e93cd2e2d5e7a1f83
fejes6 <fejesandrej@gmail.com>
2015-11-29 12:58:59 +0100

    Update IllustrativeRenderer.js

--- IllustrativeRenderer.js
+++ IllustrativeRenderer.js
@@ -63,32 +63,6 @@
     self.picked = null;
     //material wich substitutes the default mesh material when a mesh is picked
     self.pickedMaterial = new THREE.MeshNormalMaterial({color: 0x7777ff});
-    
-    
-    //***********
-    
-    
-        for (var f = 0, fl = self.pickedMaterial.geometry.faces.length; f < fl; f++) {
-        var face = self.pickedMaterial.geometry.faces[ f ];
-        var centroid = new THREE.Vector3(0, 0, 0);
-        centroid.add(self.pickedMaterial.geometry.vertices[face.a]);
-        centroid.add(self.pickedMaterial.geometry.vertices[face.b]);
-        centroid.add(self.pickedMaterial.geometry.vertices[face.c]);
-        centroid.divideScalar(3);
-
-        var arrow = new THREE.ArrowHelper(
-                face.normal,
-                centroid,
-                2,
-                0x3333FF,
-                0.5,
-                0.5);
-        self.pickedMaterial.add(arrow);
-    }
-    //***********
-    
-    
-    
     //self.pickedMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
     //self.pickedMaterial = new THREE.MeshBasicMaterial({ color: 'blue', transparent: true, blending: THREE.NoBlending });
     //color: 0x00ff00, transparent: true, blending: THREE.MultiplyBlending
