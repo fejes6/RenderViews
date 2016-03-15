9bf97bd0b95a74b79cbd4ae8276f2e08d699fea1
fejes6 <fejesandrej@gmail.com>
2016-03-15 14:26:33 +0100

    Update IllustrativeRenderer.js

--- IllustrativeRenderer.js
+++ IllustrativeRenderer.js
@@ -1,7 +1,7 @@
 'use strict'
-function IllustrativeRenderer(domQuery) { //for whole window call with domQuery "<body>"
+function IllustrativeRenderer(domQuery, basicOffscreen) { //for whole window call with domQuery "<body>"
     //A simple inhericance concept. We create an object of the AbstractRenderer type and then we change and extend it to our needs. At the end we return the self variable.
-    var self = BasicThreeRenderer(domQuery, basicOffscreen);
+    var self = BasicThreeRenderer(domQuery);
 
     //A map with three.js meshes with shape IDs as keys. Only for mergning and picking, not rendered.
     self.Meshes = {};
