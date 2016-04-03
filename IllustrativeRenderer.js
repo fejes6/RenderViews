2a9b6c36f3c6ab3217755642408e50a1fc678e66
fejes6 <fejesandrej@gmail.com>
2016-04-03 22:27:58 +0200

    Update IllustrativeRenderer.js

--- IllustrativeRenderer.js
+++ IllustrativeRenderer.js
@@ -3,7 +3,7 @@ function IllustrativeRenderer(domQuery) { //for whole window call with domQuery 
     //A simple inhericance concept. We create an object of the AbstractRenderer type and then we change and extend it to our needs. At the end we return the self variable.
     var self = BasicThreeRenderer(domQuery);
     
-    mat = new THREE.ShaderMaterial({  
+    material = new THREE.ShaderMaterial({  
       vertexShader: document.getElementById('vertexShader').textContent,
       fragmentShader: document.getElementById('fragmentShader').textContent
     });
