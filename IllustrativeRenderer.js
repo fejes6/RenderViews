2d88f15d28d0ca46595d8d6098efa9c186ea878a
fejes6 <fejesandrej@gmail.com>
2016-04-03 21:40:37 +0200

    Update IllustrativeRenderer.js

--- IllustrativeRenderer.js
+++ IllustrativeRenderer.js
@@ -2,6 +2,8 @@
 function IllustrativeRenderer(domQuery) { //for whole window call with domQuery "<body>"
     //A simple inhericance concept. We create an object of the AbstractRenderer type and then we change and extend it to our needs. At the end we return the self variable.
     var self = BasicThreeRenderer(domQuery);
+    
+    var material = new THREE.MeshBasicMaterial({color: 0x0066CC}); 
 
     return self;
 }
