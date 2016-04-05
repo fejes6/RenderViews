d9e96f403bef023241c343e913f91e5b3374dbd8
fejes6 <fejesandrej@gmail.com>
2016-04-05 19:38:22 +0200

    Update IllustrativeRenderer.js

--- IllustrativeRenderer.js
+++ IllustrativeRenderer.js
@@ -3,7 +3,7 @@ function IllustrativeRenderer(domQuery) { //for whole window call with domQuery 
     //A simple inhericance concept. We create an object of the AbstractRenderer type and then we change and extend it to our needs. At the end we return the self variable.
     var self = BasicThreeRenderer(domQuery);
    
-   var composer;
+   var composer, renderer;
    
    
    				// postprocessing
