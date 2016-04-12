abf141fd8b882eb25f095bb06f6834228ef8e783
fejes6 <fejesandrej@gmail.com>
2016-04-12 20:05:39 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -50,6 +50,7 @@ THREE.TestRedShader = {
 
 };
 */
+/****************************************************************************************************************************/
 /**
  * @author alteredq / http://alteredqualia.com/
  * @author davidedc / http://www.sketchpatch.net/
@@ -68,6 +69,7 @@ THREE.TestRedShader = {
  * https://github.com/evanw/glfx.js
  */
 
+/**
 THREE.TestRedShader = {
 
 	uniforms: {
@@ -124,6 +126,90 @@ THREE.TestRedShader = {
 			"gl_FragColor = vec4( vec3( average * 10.0 - 5.0 + pattern() ), color.a );",
 
 		"}"
+
+	].join( "\n" )
+
+};
+
+*/
+
+THREE.TestRedShader = {
+
+	uniforms: {
+
+		"sceneTex": { type: "t", value: null },
+		"vx_offset":    { type: "f" },
+		"rt_w":   { type: "f" },
+		"rt_h":    { type: "f" },
+		"hatch_y_offset":    { type: "f", value: 5.0 }
+		"lum_threshold_1":    { type: "f", value: 1.0 }
+		"lum_threshold_2":    { type: "f", value: 0.7 }
+		"lum_threshold_3":    { type: "f", value: 0.5 }
+		"lum_threshold_4":    { type: "f", value: 0.3 }
+
+	},
+
+	vertexShader: [
+		
+
+		"void main() {",
+
+			"gl_Position = ftransform();",
+			"gl_TexCoord[0] = gl_MultiTexCoord0;",
+			
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		
+		"uniform sampler2D sceneTex;", // 0
+"uniform float vx_offset;",
+"uniform float rt_w;", // GeeXLab built-in
+"uniform float rt_h;", // GeeXLab built-in
+"uniform float hatch_y_offset;", // 5.0
+"uniform float lum_threshold_1;", // 1.0
+"uniform float lum_threshold_2;", // 0.7
+"uniform float lum_threshold_3;", // 0.5
+"uniform float lum_threshold_4;", // 0.3
+"void main(){", 
+  "vec2 uv = gl_TexCoord[0].xy;",
+  
+  "vec3 tc = vec3(1.0, 0.0, 0.0);",
+  "if (uv.x &lt; (vx_offset-0.005)){",
+    "float lum = length(texture2D(sceneTex, uv).rgb);",
+    "tc = vec3(1.0, 1.0, 1.0);",
+  
+    "if (lum &lt; lum_threshold_1){",
+      "if (mod(gl_FragCoord.x + gl_FragCoord.y, 10.0) == 0.0)", 
+        "tc = vec3(0.0, 0.0, 0.0);",
+    "}",  
+  
+    "if (lum &lt; lum_threshold_2){",
+      "if (mod(gl_FragCoord.x - gl_FragCoord.y, 10.0) == 0.0)", 
+        "tc = vec3(0.0, 0.0, 0.0);",
+    "}",  
+  
+    "if (lum &lt; lum_threshold_3){",
+      "if (mod(gl_FragCoord.x + gl_FragCoord.y - hatch_y_offset, 10.0) == 0.0)", 
+        "tc = vec3(0.0, 0.0, 0.0);",
+    "}",  
+  
+    "if (lum &lt; lum_threshold_4){",
+      "if (mod(gl_FragCoord.x - gl_FragCoord.y - hatch_y_offset, 10.0) == 0.0)", 
+        "tc = vec3(0.0, 0.0, 0.0);",
+    "}",
+  "}",
+  "else if (uv.x&gt;=(vx_offset+0.005)){",
+    "tc = texture2D(sceneTex, uv).rgb;",
+  "}",
+  
+  "gl_FragColor = vec4(tc, 1.0);",
+"}"
+
+
+		
 
 	].join( "\n" )
 
