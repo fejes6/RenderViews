c0360b86b53bf9325777658bffbaf5aca0308566
fejes6 <fejesandrej@gmail.com>
2016-04-16 11:07:06 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -46,68 +46,92 @@
  	
  uniforms: {
 
-		"tDiffuse": { type: "t", value: null },
-		"LightPosition": { type: "v3", value: new THREE.Vector3(0, 500, 0) },
-		"SurfaceColor": { type: "v3", value: new THREE.Vector3(0, 1, 1) },
-		"WarmColor": { type: "v3", value: new THREE.Vector3(1, 0, 1) },
-		"CoolColor": { type: "v3", value: new THREE.Vector3(1, 1, 0) },
-		"DiffuseWarm": { type: "f", value: 0.45},
-		"DiffuseCool": { type: "f", value: 0.45}
+		"Projection": { type: "m4", value: new THREE.Matrix4() },
+		"Modelview": { type: "m4", value: new THREE.Matrix4() },
+		"NormalMatrix": { type: "m4", value: new THREE.Matrix4() },
+		"DiffuseMaterial": { type: "v3", value: new THREE.Vector3(1, 0, 1) },
+		"LightPosition": { type: "v3", value: new THREE.Vector3(0, 0, 1) },
+		"AmbientMaterial": {  type: "v3", value: new THREE.Vector3(0, 1, 0) },
+		"SpecularMaterial": { type: "v3", value: new THREE.Vector3(1, 0, 0) }
+		"Shininess": { type: "f", value: 0.45}
 	},
 
 	vertexShader: [
 
-		"uniform vec3 LightPosition;",
-		
-		"varying float NdotL;",
-		"varying vec3 ReflectVec;",
-		"varying vec3 ViewVec;",
-		"varying vec2 vUv;",
-		
-		"void main() {",
-
-			"vec3 EyePos = (modelViewMatrix * vec4(position, 1.0)).xyz;",
-			"vec3 trans_norm = normalize(normalMatrix * normal);",
-			"vec3 lightVec 	= normalize(LightPosition - EyePos);",
-			"ReflectVec    	= normalize(reflect(-lightVec, trans_norm));",
-			"ViewVec       	= normalize(-EyePos);",
-			"NdotL         	= (dot (lightVec, trans_norm) + 1.0) * 0.5;",
-			"vUv = uv;",
-			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-
-		"}"
+	"attribute vec4 Position;",
+	"attribute vec3 Normal;",
+	
+	"uniform mat4 Projection;",
+	"uniform mat4 Modelview;",
+	"uniform mat3 NormalMatrix;",
+	"uniform vec3 DiffuseMaterial;",
+	
+	"varying vec3 EyespaceNormal;",
+	"varying vec3 Diffuse;",
+	
+	"void main()",
+	"{",
+	    "EyespaceNormal = NormalMatrix * Normal;",
+	    "gl_Position = Projection * Modelview * Position;",
+	    "Diffuse = DiffuseMaterial;",
+	"}"
 
 	].join("\n"),
 
 	fragmentShader: [
 
-		"uniform vec3  SurfaceColor;",
-		"uniform vec3  WarmColor;",
-		"uniform vec3  CoolColor;",
-		"uniform float DiffuseWarm;",
-		"uniform float DiffuseCool;",
-		"uniform sampler2D tDiffuse;",
-
-		"varying float NdotL;",
-		"varying vec3  ReflectVec;",
-		"varying vec3  ViewVec;",
-		"varying vec2 vUv;",
-
-		"void main(void) {",
-		
-		"vec4 color = texture2D(tDiffuse, vUv);",
-		  "vec3 kcool    = min (CoolColor + DiffuseCool * SurfaceColor, 1.0);",
-		  "vec3 kwarm    = min (WarmColor + DiffuseWarm * SurfaceColor, 1.0);",
-		  "vec3 kfinal   = mix (kcool, kwarm, NdotL);",
-
-		  "vec3 nreflect = normalize (ReflectVec);",
-		  "vec3 nview    = normalize (ViewVec);",
-
-		  "float spec    = max (dot (nreflect, nview), 0.0);",
-		  "spec          = pow (spec, 32.0);",
-
-		  "gl_FragColor  = vec4 (min (kfinal + spec, 1.0), color.a);",
-		"}"
+	"varying vec3 EyespaceNormal;",
+	"varying vec3 Diffuse;",
+	
+	"uniform vec3 LightPosition;",
+	"uniform vec3 AmbientMaterial;",
+	"uniform vec3 SpecularMaterial;",
+	"uniform float Shininess;",
+	
+	"float stepmix(float edge0, float edge1, float E, float x)",
+	"{",
+	    "float T = clamp(0.5 * (x - edge0) / E, 0.0, 1.0);",
+	    "return mix(edge0, edge1, T);",
+	"}",
+	
+	"void main()",
+	"{",
+	    "vec3 N = normalize(EyespaceNormal);",
+	    "vec3 L = normalize(LightPosition);",
+	    "vec3 Eye = vec3(0, 0, 1);",
+	    "vec3 H = normalize(L + Eye);",
+	    
+	    "float df = max(0.0, dot(N, L));",
+	    "float sf = max(0.0, dot(N, H));",
+	    "sf = pow(sf, Shininess);",
+	
+	    "const float A = 0.1;",
+	    "const float B = 0.3;",
+	    "const float C = 0.6;",
+	    "const float D = 1.0;",
+	    "float E = fwidth(df);",
+	
+	    "if      (df > A - E && df < A + E) df = stepmix(A, B, E, df);",
+	    "else if (df > B - E && df < B + E) df = stepmix(B, C, E, df);",
+	    "else if (df > C - E && df < C + E) df = stepmix(C, D, E, df);",
+	    "else if (df < A) df = 0.0;",
+	    "else if (df < B) df = B;",
+	    "else if (df < C) df = C;",
+	    "else df = D;",
+	
+	    "E = fwidth(sf);",
+	    "if (sf > 0.5 - E && sf < 0.5 + E)",
+	    "{",
+	        "sf = clamp(0.5 * (sf - 0.5 + E) / E, 0.0, 1.0);",
+	    ""}",
+	    "else",
+	   "{",
+	        "sf = step(0.5, sf);",
+	    "}",
+	
+	    "vec3 color = AmbientMaterial + df * Diffuse + sf * SpecularMaterial;",
+	    "gl_FragColor = vec4(color, 1.0);",
+	"}"
 
 	].join("\n")
 
