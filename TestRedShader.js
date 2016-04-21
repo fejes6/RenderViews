dbd7a9e01110db0234cd394b48f0b477898df342
fejes6 <fejesandrej@gmail.com>
2016-04-21 20:13:06 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -43,10 +43,9 @@
 };
 */
 
- THREE.TestRedShader = {
- 	
 
-//THREE.SepiaShader = {
+/*
+THREE.SepiaShader = {
 
 	uniforms: {
 
@@ -89,6 +88,54 @@
 
 		"}"
 
+	].join("\n")
+
+};
+*/
+
+ THREE.TestRedShader = {
+ 	
+ 	uniforms: {
+ 	"lightDir":	{ type: "v3", value: new THREE.Vector3() }
+ 	},
+
+	vertexShader: [
+		"varying vec3 normal;",
+
+		"void main()",
+		"{",
+			"normal = gl_Normal;",
+			"gl_Position = ftransform();",
+		
+		"}"
+		
+		
+	].join("\n"),
+
+	fragmentShader: [
+		"uniform vec3 lightDir;",
+		"varying vec3 normal;",
+		
+		"void main()",
+		"{",
+			"float intensity;",
+			"vec4 color;",
+			"intensity = dot(lightDir,normal);",
+		
+			"if (intensity > 0.95)",
+				"color = vec4(1.0,0.5,0.5,1.0);",
+			"else if (intensity > 0.5)",
+				"color = vec4(0.6,0.3,0.3,1.0);",
+			"else if (intensity > 0.25)",
+				"color = vec4(0.4,0.2,0.2,1.0);",
+			"else",
+				"color = vec4(0.2,0.1,0.1,1.0);",
+			"gl_FragColor = color;",
+		
+		"}"
+		
+		
+		
 	].join("\n")
 
 };
