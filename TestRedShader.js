a134f8f1fc62ecf812aa8e11e4f3ae5f2bf1d581
fejes6 <fejesandrej@gmail.com>
2016-04-21 19:46:56 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -45,98 +45,78 @@
 
  THREE.TestRedShader = {
  	
- uniforms: {
+// THREE.ShaderToon = {
 
-		"Projection": { type: "m4", value: new THREE.Matrix4() },
-		"Modelview": { type: "m4", value: new THREE.Matrix4() },
-		"NormalMatrix": { type: "m4", value: new THREE.Matrix4() },
-		"DiffuseMaterial": { type: "v3", value: new THREE.Vector3(1, 0, 1) },
-		"LightPosition": { type: "v3", value: new THREE.Vector3(0, 0, 1) },
-		"AmbientMaterial": {  type: "v3", value: new THREE.Vector3(0, 1, 0) },
-		"SpecularMaterial": { type: "v3", value: new THREE.Vector3(1, 0, 0) },
-		"Shininess": { type: "f", value: 0.45}
+	uniforms: {
+
+		"uDirLightPos":	{ type: "v3", value: new THREE.Vector3() },
+		"uDirLightColor": { type: "c", value: new THREE.Color( 0xeeeeee ) },
+
+		"uAmbientLightColor": { type: "c", value: new THREE.Color( 0x050505 ) },
+
+		"uBaseColor":  { type: "c", value: new THREE.Color( 0xffffff ) }
+
 	},
 
 	vertexShader: [
 
-	"attribute vec4 Position;",
-	"attribute vec3 Normal;",
-	
-	"uniform mat4 Projection;",
-	"uniform mat4 Modelview;",
-	"uniform mat3 NormalMatrix;",
-	"uniform vec3 DiffuseMaterial;",
-	
-	"varying vec3 EyespaceNormal;",
-	"varying vec3 Diffuse;",
-	
-	"void main()",
-	"{",
-	    "EyespaceNormal = NormalMatrix * Normal;",
-	    "gl_Position = Projection * Modelview * Position;",
-	    "Diffuse = DiffuseMaterial;",
-	"}"
+		"varying vec3 vNormal;",
+		"varying vec3 vRefract;",
+
+		"void main() {",
+
+			"vec4 worldPosition = modelMatrix * vec4( position, 1.0 );",
+			"vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
+			"vec3 worldNormal = normalize ( mat3( modelMatrix[0].xyz, modelMatrix[1].xyz, modelMatrix[2].xyz ) * normal );",
+
+			"vNormal = normalize( normalMatrix * normal );",
+
+			"vec3 I = worldPosition.xyz - cameraPosition;",
+			"vRefract = refract( normalize( I ), worldNormal, 1.02 );",
+
+			"gl_Position = projectionMatrix * mvPosition;",
+
+		"}"
 
 	].join("\n"),
 
 	fragmentShader: [
-	"#extension GL_OES_standard_derivatives : enable",
 
-	"varying vec3 EyespaceNormal;",
-	"varying vec3 Diffuse;",
-	
-	"uniform vec3 LightPosition;",
-	"uniform vec3 AmbientMaterial;",
-	"uniform vec3 SpecularMaterial;",
-	"uniform float Shininess;",
-	
-	"float stepmix(float edge0, float edge1, float E, float x)",
-	"{",
-	    "float T = clamp(0.5 * (x - edge0) / E, 0.0, 1.0);",
-	    "return mix(edge0, edge1, T);",
-	"}",
-	
-	"void main()",
-	"{",
-	    "vec3 N = normalize(EyespaceNormal);",
-	    "vec3 L = normalize(LightPosition);",
-	    "vec3 Eye = vec3(0, 0, 1);",
-	    "vec3 H = normalize(L + Eye);",
-	    
-	    "float df = max(0.0, dot(N, L));",
-	    "float sf = max(0.0, dot(N, H));",
-	    "sf = pow(sf, Shininess);",
-	
-	    "const float A = 0.1;",
-	    "const float B = 0.3;",
-	    "const float C = 0.6;",
-	    "const float D = 1.0;",
-	    "float E = fwidth(df);",
-	
-	    "if      (df > A - E && df < A + E) df = stepmix(A, B, E, df);",
-	    "else if (df > B - E && df < B + E) df = stepmix(B, C, E, df);",
-	    "else if (df > C - E && df < C + E) df = stepmix(C, D, E, df);",
-	    "else if (df < A) df = 0.0;",
-	    "else if (df < B) df = B;",
-	    "else if (df < C) df = C;",
-	    "else df = D;",
-	
-	    "E = fwidth(sf);",
-	    "if (sf > 0.5 - E && sf < 0.5 + E)",
-	    "{",
-	        "sf = clamp(0.5 * (sf - 0.5 + E) / E, 0.0, 1.0);",
-	    "}",
-	    "else",
-	   "{",
-	        "sf = step(0.5, sf);",
-	    "}",
-	
-	    "vec3 color = AmbientMaterial + df * Diffuse + sf * SpecularMaterial;",
-	    "gl_FragColor = vec4(color, 1.0);",
-	"}"
+		"uniform vec3 uBaseColor;",
+
+		"uniform vec3 uDirLightPos;",
+		"uniform vec3 uDirLightColor;",
+
+		"uniform vec3 uAmbientLightColor;",
+
+		"varying vec3 vNormal;",
+
+		"varying vec3 vRefract;",
+
+		"void main() {",
+
+			"float directionalLightWeighting = max( dot( normalize( vNormal ), uDirLightPos ), 0.0);",
+			"vec3 lightWeighting = uAmbientLightColor + uDirLightColor * directionalLightWeighting;",
+
+			"float intensity = smoothstep( - 0.5, 1.0, pow( length(lightWeighting), 20.0 ) );",
+			"intensity += length(lightWeighting) * 0.2;",
+
+			"float cameraWeighting = dot( normalize( vNormal ), vRefract );",
+			"intensity += pow( 1.0 - length( cameraWeighting ), 6.0 );",
+			"intensity = intensity * 0.2 + 0.3;",
+
+			"if ( intensity < 0.50 ) {",
+
+				"gl_FragColor = vec4( 2.0 * intensity * uBaseColor, 1.0 );",
+
+			"} else {",
+
+				"gl_FragColor = vec4( 1.0 - 2.0 * ( 1.0 - intensity ) * ( 1.0 - uBaseColor ), 1.0 );",
+
+			"}",
+
+		"}"
 
 	].join("\n")
 
-};
-
-
+},
