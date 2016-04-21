65dc8c9f30011d06b34d5a83a73a22f43f632625
fejes6 <fejesandrej@gmail.com>
2016-04-21 22:36:12 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -92,7 +92,7 @@ THREE.SepiaShader = {
 
 };
 */
-/*
+
  THREE.TestRedShader = {
  	
  	uniforms: {
@@ -145,84 +145,3 @@ THREE.SepiaShader = {
 	].join("\n")
 
 };
-*/
-
-THREE.TestRedShader = {
-
-				uniforms: {
-
-					"uDirLightPos":	{ type: "v3", value: new THREE.Vector3() },
-					"uDirLightColor": { type: "c", value: new THREE.Color( 0xffffff ) },
-					"tDiffuse": { type: "t", value: null },
-
-					//"uMaterialColor":  { type: "c", value: new THREE.Color( 0xffffff ) },
-
-					uKd: {
-						type: "f",
-						value: 0.7
-					},
-					uBorder: {
-						type: "f",
-						value: 0.4
-					}
-				},
-
-				vertexShader: [
-					
-					"varying vec2 vUv;",
-					"varying vec3 vNormal;",
-					"varying vec3 vViewPosition;",
-
-					"void main() {",
-					"vUv = uv;",
-
-						"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
-						"vNormal = normalize( normalMatrix * normal );",
-						"vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );",
-						"vViewPosition = -mvPosition.xyz;",
-
-					"}"
-
-				].join("\n"),
-
-				fragmentShader: [
-					"uniform sampler2D tDiffuse;",
-
-
-					//"uniform vec3 uMaterialColor;",
-
-					"uniform vec3 uDirLightPos;",
-					"uniform vec3 uDirLightColor;",
-
-					"uniform float uKd;",
-					"uniform float uBorder;",
-
-					"varying vec3 vNormal;",
-					"varying vec3 vViewPosition;",
-					"varying vec2 vUv;",
-
-					"void main() {",
-					"vec4 uMaterialColor = texture2D(tDiffuse, vUv);",
-
-						// compute direction to light
-						"vec4 lDirection = viewMatrix * vec4( uDirLightPos, 0.0 );",
-						"vec3 lVector = normalize( lDirection.xyz );",
-
-						// diffuse: N * L. Normal must be normalized, since it's interpolated.
-						"vec3 normal = normalize( vNormal );",
-						//was: "float diffuse = max( dot( normal, lVector ), 0.0);",
-						// solution
-						"float diffuse = dot( normal, lVector );",
-						"if ( diffuse > 0.6 ) { diffuse = 1.0; }",
-						"else if ( diffuse > -0.2 ) { diffuse = 0.7; }",
-						"else { diffuse = 0.3; }",
-
-						"gl_FragColor = vec4( uKd * uMaterialColor * uDirLightColor * diffuse, 1.0 );",
-
-					"}"
-
-				].join("\n")
-
-			
-
-			};
