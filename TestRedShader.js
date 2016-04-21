cb9fb5db26ff98a880699dda4b2ebda6d0c5673e
fejes6 <fejesandrej@gmail.com>
2016-04-21 21:55:08 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -96,45 +96,48 @@ THREE.SepiaShader = {
  THREE.TestRedShader = {
  	
  	uniforms: {
- 		"tDiffuse": { type: "t", value: null },
- 		"lightDir":	{ type: "v3", value: new THREE.Vector3() }
+ 		"texture": { type: "t", value: null },
+ 		//"lightDir":	{ type: "v3", value: new THREE.Vector3() }
  	
  	},
 
 	vertexShader: [
-	"varying vec2 vUv;",
-	"varying vec3 normal;",
-	"void main() {",
-		"vUv = uv;",
-		"normal = gl_Normal;",
-		"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+		"varying vec3 normal, lightDir;",
+		"varying vec2 texCoord;",
 		
-	"}"
+		"void main(){",
+			"vec4 ecPos;",
+			"ecPos = vec4(gl_ModelViewMatrix * gl_Vertex);",
+			"lightDir = normalize(vec3(gl_LightSource[0].position) - ecPos.xyz);",
+			"normal = normalize(gl_NormalMatrix * gl_Normal);",
+			
+			"texCoord = vec2(gl_MultiTexCoord0);",
+			"gl_Position = ftransform();",
+		"}"
 	].join("\n"),
 
 	fragmentShader: [
-		"uniform vec3 lightDir;",
-		"uniform sampler2D tDiffuse;",
-		"varying vec3 normal;",
-		"varying vec2 vUv;",
+		"varying vec3 normal, lightDir;",
+		"varying vec2 texCoord;",
+		"uniform sampler2D texture;",
 		
-		"void main()",
-		"{",
+		"void main(){",
 			"float intensity;",
-			"vec4 color= texture2D( tDiffuse, vUv );",
+			"vec3 n;",
+			"vec4 _color;",
 			
-			"intensity = dot(lightDir,normal);",
-		
-			"if (intensity > 0.95)",
-				"color = vec4(1.0,0.5,0.5,1.0);",
+			"n = normalize(normal);",
+			"intensity = dot(lightDir, n);",
+			
+			"if (intensity > 0.98)",
+			"_color = vec4(1.0,1.0,1.0,1.0);",
 			"else if (intensity > 0.5)",
-				"color = vec4(0.6,0.3,0.3,1.0);",
-			"else if (intensity > 0.25)",
-				"color = vec4(0.4,0.2,0.2,1.0);",
+			"_color = vec4(0.8,0.8,0.8,1.0);",
+			"else if (intensity > 0.35)",
+			"_color = vec4(0.4,0.4,0.4,1.0);",
 			"else",
-				"color = vec4(0.2,0.1,0.1,1.0);",
-			"gl_FragColor = color;",
-		
+			"_color = vec4(0.0,0.0,0.0,1.0);",
+			"gl_FragColor = _color * texture2D(texture, texCoord);",
 		"}"
 		
 		
