5f58b50d46871a20916f0f1bd351f819076c3c19
fejes6 <fejesandrej@gmail.com>
2016-04-22 16:08:57 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -147,7 +147,7 @@ THREE.SepiaShader = {
 
 };
 */
-
+/* toon shader bez erroru ale iba cierna obrazovka
 THREE.TestRedShader = {
 	
 		
@@ -224,3 +224,54 @@ THREE.TestRedShader = {
 			
 
 			};
+*/
+
+THREE.TestRedShader = {
+	
+				uniforms: {
+					"tDiffuse": { type: "t", value: null },
+					"lightDir":	{ type: "v3", value: new THREE.Vector3() },
+				},
+
+				vertexShader: [
+					
+					"varying vec3 normal;",
+					"varying vec2 vUv;",
+					
+					"void main()",
+					"{",
+						"normal = gl_Normal;",
+						"vUv = uv;",
+						"gl_Position = ftransform();",
+					
+					"}"
+			].join("\n"),
+
+				fragmentShader: [
+					"uniform sampler2D tDiffuse;",
+					"uniform vec3 lightDir;",
+					"varying vec3 normal;",
+					"varying vec2 vUv;",
+					
+					"void main()",
+					"{",
+						"float intensity;",
+						"vec4 color;",
+						"intensity = dot(lightDir,normal);",
+					
+						"if (intensity > 0.95)",
+							"color = vec4(1.0,0.5,0.5,1.0);",
+						"else if (intensity > 0.5)",
+							"color = vec4(0.6,0.3,0.3,1.0);",
+						"else if (intensity > 0.25)",
+							"color = vec4(0.4,0.2,0.2,1.0);",
+						"else",
+							"color = vec4(0.2,0.1,0.1,1.0);",
+						"gl_FragColor = color * texture2D( tDiffuse, vUv );",
+					
+					"}"
+			
+			
+			].join("\n")
+
+};
