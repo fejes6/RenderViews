c93f798db662f9f318ed23afc12e2be3a10a76a4
fejes6 <fejesandrej@gmail.com>
2016-04-23 16:29:39 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -44,8 +44,8 @@
 */
 
 
-
-THREE.TestRedShader = {
+/*
+THREE.SepiaShader = {
 
     uniforms: {
 
@@ -91,7 +91,7 @@ THREE.TestRedShader = {
     ].join("\n")
 
 };
-
+*/
 /*
 // normalize by malo ist
 // toon shader bez erroru ale iba cierna obrazovka
@@ -171,7 +171,84 @@ THREE.TestRedShader = {
             
 
             };
-
 */
 
+THREE.TestRedShader = {
+    
+        
+                uniforms: {
+
+                    "tDiffuse": { type: "t", value: null },
+                    		"uDirLightPos":	{ type: "v3", value: new THREE.Vector3() },
+                    		"uDirLightColor": { type: "c", value: new THREE.Color( 0xeeeeee ) },
+                    
+                    		"uAmbientLightColor": { type: "c", value: new THREE.Color( 0x050505 ) },
+                    
+                    		//"uBaseColor":  { type: "c", value: new THREE.Color( 0xeeeeee ) },
+                    		"uLineColor1": { type: "c", value: new THREE.Color( 0x808080 ) },
+                    		"uLineColor2": { type: "c", value: new THREE.Color( 0x000000 ) },
+                    		"uLineColor3": { type: "c", value: new THREE.Color( 0x000000 ) },
+                    		"uLineColor4": { type: "c", value: new THREE.Color( 0x000000 ) }
+                },
+
+                vertexShader: [
+
+              		"varying vec3 vNormal;",
+              		"varying vec2 vUv;",
+              
+              		"void main() {",
+              "vUv = uv;",
+              			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+              			"vNormal = normalize( normalMatrix * normal );",
+              
+              		"}"
+
+                ].join("\n"),
+
+                fragmentShader: [
+                 
+                 "uniform sampler2D tDiffuse;",
+
+                 		//"uniform vec3 uBaseColor;",
+                 		"uniform vec3 uLineColor1;",
+                 		"uniform vec3 uLineColor2;",
+                 		"uniform vec3 uLineColor3;",
+                 		"uniform vec3 uLineColor4;",
+                 
+                 		"uniform vec3 uDirLightPos;",
+                 		"uniform vec3 uDirLightColor;",
+                 
+                 		"uniform vec3 uAmbientLightColor;",
+                 
+                 		"varying vec3 vNormal;",
+                 		"varying vec2 vUv;",
+                 
+                 		"void main() {",
+                 
+                 			"float camera = max( dot( normalize( vNormal ), vec3( 0.0, 0.0, 1.0 ) ), 0.4);",
+                 			"float light = max( dot( normalize( vNormal ), uDirLightPos ), 0.0);",
+                 			
+                 			"vec4 uBaseColor = texture2D(tDiffuse, vUv);",
+                 
+                 			"gl_FragColor = vec4( uBaseColor.rgb, 1.0 );",
+                 
+                 			"if ( length(uAmbientLightColor + uDirLightColor * light) < 1.00 ) {",
+                 
+                 				"gl_FragColor *= vec4( uLineColor1, 1.0 );",
+                 
+                 			"}",
+                 
+                 			"if ( length(uAmbientLightColor + uDirLightColor * camera) < 0.50 ) {",
+                 
+                 				"gl_FragColor *= vec4( uLineColor2, 1.0 );",
+                 
+                 			"}",
+                 
+                 		"}"
+
+                ].join("\n")
+
+            
+
+            };
 
