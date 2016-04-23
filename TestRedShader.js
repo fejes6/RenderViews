733d86dbcfb6d48328be65be7c1673c5374db85c
fejes6 <fejesandrej@gmail.com>
2016-04-23 12:06:39 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -1,7 +1,10 @@
 THREE.TestRedShader = {
 	uniforms: {
 		"tDiffuse": { type: "t", value: null },
+		"tDepth": { type: "t", value: rtScene },
+		"tBlurDepth": { type: "t", value: rtDepth },
 		"delta": {type: "v2", value: new THREE.Vector2(1.0 / window.innerWidth, 1.0 / window.innerHeight)},
+		"intensity": {type: "f", value:0.7},
 	},
 	vertexShader: [
 		"varying vec2 vUv;",
@@ -13,21 +16,18 @@ THREE.TestRedShader = {
 	fragmentShader: [
 		"varying vec2 vUv;",
 		"uniform sampler2D tDiffuse;",
+		"uniform sampler2D tDepth;",
+		"uniform sampler2D tBlurDepth;",
+		"uniform float intensity;",
+
 		"uniform vec2 delta;",
 
-		//yeah, could have done it seperated...
 		"void main() {",
-			"gl_FragColor =	texture2D( tDiffuse, vUv);",
-			"gl_FragColor.rgb = gl_FragColor.rgb * 0.25 +",
-				"texture2D( tDiffuse, vUv  + delta).rgb * 0.0625 +",
-				"texture2D( tDiffuse, vUv  - delta).rgb * 0.0625 +",
-				"texture2D( tDiffuse, vUv  + delta * vec2(1, -1)).rgb * 0.0625 +",
-				"texture2D( tDiffuse, vUv  + delta * vec2(- 1,1)).rgb * 0.0625 +",
-
-				"texture2D( tDiffuse, vUv  + delta * vec2(1, 0)).rgb * 0.125 +",
-				"texture2D( tDiffuse, vUv  - delta * vec2(0, 1)).rgb * 0.125 +",
-				"texture2D( tDiffuse, vUv  + delta * vec2(-1,0)).rgb * 0.125 +",
-				"texture2D( tDiffuse, vUv  + delta * vec2(0,-1)).rgb * 0.125;",
+			"float depthOrig = texture2D( tDepth, vUv ).b;",
+			"float depthBlur = texture2D( tBlurDepth, vUv ).b;",
+			"float diff = (-depthOrig + depthBlur) * intensity;",
+			"gl_FragColor = texture2D( tDiffuse, vUv );",
+			"gl_FragColor.rgb -= vec3(abs(diff));",
 		"}"
 	].join( "\n" )
 };
