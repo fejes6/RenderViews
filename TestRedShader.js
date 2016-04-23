80478e374bddbe8c2a1c24771a682b194fda7be6
fejes6 <fejesandrej@gmail.com>
2016-04-23 11:35:10 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -1,6 +1,7 @@
 THREE.TestRedShader = {
 	uniforms: {
 		"tDiffuse": { type: "t", value: null },
+		"delta": {type: "v2", value: new THREE.Vector2(1.0 / window.innerWidth, 1.0 / window.innerHeight)},
 	},
 	vertexShader: [
 		"varying vec2 vUv;",
@@ -10,12 +11,23 @@ THREE.TestRedShader = {
 		"}"
 	].join( "\n" ),
 	fragmentShader: [
-		"varying vec2 vUv;",
+varying vec2 vUv;
 		"uniform sampler2D tDiffuse;",
+		"uniform vec2 delta;",
 
+		//yeah, could have done it seperated...
+		"void main() {
+			"gl_FragColor =	texture2D( tDiffuse, vUv);",
+			"gl_FragColor.rgb = gl_FragColor.rgb * 0.25 +",
+				"texture2D( tDiffuse, vUv  + delta).rgb * 0.0625 +",
+				"texture2D( tDiffuse, vUv  - delta).rgb * 0.0625 +",
+				"texture2D( tDiffuse, vUv  + delta * vec2(1, -1)).rgb * 0.0625 +",
+				"texture2D( tDiffuse, vUv  + delta * vec2(- 1,1)).rgb * 0.0625 +",
 
-		"void main() {",
-			"gl_FragColor = vec4(texture2D( tDiffuse, vUv ).r, 1.0);",
+				"texture2D( tDiffuse, vUv  + delta * vec2(1, 0)).rgb * 0.125 +",
+				"texture2D( tDiffuse, vUv  - delta * vec2(0, 1)).rgb * 0.125 +",
+				"texture2D( tDiffuse, vUv  + delta * vec2(-1,0)).rgb * 0.125 +",
+				"texture2D( tDiffuse, vUv  + delta * vec2(0,-1)).rgb * 0.125;",
 		"}"
 	].join( "\n" )
 };
