a0b97a7ca1fedb1af9641d509d306b1e0e96e156
fejes6 <fejesandrej@gmail.com>
2016-04-23 12:24:26 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -1,10 +1,10 @@
 THREE.TestRedShader = {
 	uniforms: {
 		"tDiffuse": { type: "t", value: null },
-		"tDepth": { type: "t", value: null },
-		"tBlurDepth": { type: "t", value: null },
 		"delta": {type: "v2", value: new THREE.Vector2(1.0 / window.innerWidth, 1.0 / window.innerHeight)},
-		"intensity": {type: "f", value:0.7},
+		"colorLo": {type: 'v3', value: new THREE.Vector3( 0.329,0.152,0.376 )},
+		"colorHi": {type: 'v3', value: new THREE.Vector3( 0.96, 0.68, 0.28 )},
+		"steps": { type: 'f', value: 0.0},
 	},
 	vertexShader: [
 		"varying vec2 vUv;",
@@ -14,20 +14,24 @@ THREE.TestRedShader = {
 		"}"
 	].join( "\n" ),
 	fragmentShader: [
-		"varying vec2 vUv;",
+varying vec2 vUv;
 		"uniform sampler2D tDiffuse;",
-		"uniform sampler2D tDepth;",
-		"uniform sampler2D tBlurDepth;",
-		"uniform float intensity;",
-
 		"uniform vec2 delta;",
 
+		"uniform vec3 colorLo;",
+		"uniform vec3 colorHi;",
+
+		"uniform float steps;",
+
 		"void main() {",
-			"float depthOrig = texture2D( tDepth, vUv ).b;",
-			"float depthBlur = texture2D( tBlurDepth, vUv ).b;",
-			"float diff = (-depthOrig + depthBlur) * intensity;",
-			"gl_FragColor = texture2D( tDiffuse, vUv );",
-			"gl_FragColor.rgb -= vec3(abs(diff));",
+			"vec4 source = texture2D( tDiffuse, vUv);",
+			"float illu = source.r;",
+			"if (steps == 1.0)",
+				"illu = 0.5;",
+			"else",
+				"if (steps > 1.0)",
+					"illu = floor(clamp(illu * steps, 0.0, steps - 1.0))/(steps - 1.0);",
+			"gl_FragColor = vec4(mix(colorLo, colorHi, illu), source.a);",
 		"}"
 	].join( "\n" )
 };
