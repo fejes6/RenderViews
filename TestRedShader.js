543a0957e61563afab96d24c3fd31485ef946022
fejes6 <fejesandrej@gmail.com>
2016-05-01 22:48:05 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -4,7 +4,7 @@
  * Adjust brightness of image with an 'amount' Uniform
  * @author felixturner / http://airtight.cc/
  */
-
+/*
  THREE.TestRedShader = {
 
     uniforms: {
@@ -42,7 +42,7 @@
     ].join("\n")
 
 };
-
+*/
 
 
 /*
@@ -294,3 +294,58 @@ uniforms: {
 };
 */
 
+ THREE.TestRedShader = {
+
+    uniforms: {
+        "tDiffuse": { type: "t", value: null },
+        //"amount":     { type: "f", value: 0.25 }
+    },
+
+    vertexShader: [
+
+//    "varying vec2 vUv;",
+//    "void main() {",
+//        "vUv = uv;",
+//        "gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+"varying float intensity;",
+"void main() {",
+	"vec3 lightDir = normalize(vec3(gl_LightSource[0].position));",
+	"intensity = dot(lightDir,gl_Normal);",
+
+	"gl_Position = ftransform(projectionMatrix * modelViewMatrix * vec4( position, 1.0 ));",
+
+    "}"
+
+    ].join("\n"),
+
+    fragmentShader: [
+
+ //   "uniform sampler2D tDiffuse;",
+  //  "varying vec2 vUv;",
+
+ //   "void main() {",
+
+   //     "vec4 color = texture2D(tDiffuse, vUv);",
+  //      "gl_FragColor = vec4(0.0, color.g, 0.0, 0.3);",
+"varying float intensity;",
+
+"void main() {",
+	"vec4 color;",
+	"if (intensity > 0.95)",
+
+		"color = vec4(1.0,0.5,0.5,1.0);",
+	"else if (intensity > 0.5)",
+		"color = vec4(0.6,0.3,0.3,1.0);",
+	"else if (intensity > 0.25)",
+		"color = vec4(0.4,0.2,0.2,1.0);",
+	"else",
+		"color = vec4(0.2,0.1,0.1,1.0);",
+	"gl_FragColor = color;",
+
+
+    "}"
+
+
+    ].join("\n")
+
+};
