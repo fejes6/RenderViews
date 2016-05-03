7bd6b1affff30202629cde959bca5bebfff9f864
fejes6 <fejesandrej@gmail.com>
2016-05-03 21:22:12 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -334,11 +334,10 @@ uniforms: {
 //			"color = vec4(0.4,0.2,0.2,1.0);",
 //		"else",
 //			"color = vec4(0.2,0.1,0.1,1.0);",
-//		"gl_FragColor = color;",
+		"gl_FragColor = color;",
 
 
     "}"
-
 
     ].join("\n")
 
