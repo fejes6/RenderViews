6ea356c17aee7cd0aae937412610fbeb0e3c4f05
fejes6 <fejesandrej@gmail.com>
2016-05-13 22:05:00 +0200

    Update TestRedShader.js

--- TestRedShader.js
+++ TestRedShader.js
@@ -356,7 +356,7 @@ uniforms: {
 
 		"tDiffuse": { type: "t", value: null },
 		//"glowColor": { type: "c", value: new THREE.Color(0x84ccff) },
-		//"p": { type: "f", value: 2 },
+		"EdgeFalloff": { type: "f", value: 0.5 },
 		"c": { type: "f", value: 1.0 },
 	"p": { type: "f", value: 3 },
 	"glowColor": { type: "c", value: new THREE.Color(0x84ccff) },
@@ -368,7 +368,9 @@ uniforms: {
 		"uniform float c;",
 
 		
-
+"attribute vec4 a_vertex;",
+"uniform mat3 normal_matrix;",
+"attribute vec3 a_normal;", 
 	
 
 		"varying vec2 vUv;",
@@ -379,24 +381,28 @@ uniforms: {
 "void main()",
 "{",
     //Transform vertex by modelview and projection matrices
-    "gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;",
+	"vUv = uv;",
+		"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
      
     // Position in clip space
-    "P = vec3(gl_ModelViewMatrix * gl_Vertex);",
+    "P = vec3(modelViewMatrix * a_vertex);",
      
     // Normal transform (transposed model-view inverse)
-    "N = gl_NormalMatrix * gl_Normal;",
+    "N = normal_matrix * a_normal;",
      
     // Incident vector
     "I = P;",
      
     // Forward current color and texture coordinates after applying texture matrix
-    "gl_FrontColor = gl_Color;",
-    "gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;",
+//    "gl_FrontColor = gl_Color;",
+//    "gl_TexCoord[0] = gl_TextureMatrix[0] * gl_MultiTexCoord0;",
 "}"
 	].join("\n"),
 
 	fragmentShader: [
+		
+"uniform sampler2D tDiffuse;",
+    "varying vec2 vUv;",
 
 "varying vec3 P;",
 "varying vec3 N;",
@@ -406,11 +412,12 @@ uniforms: {
  
 "void main()",
 "{",
+"vec4 color = texture2D(tDiffuse, vUv);",
     "float opacity = dot(normalize(N), normalize(-I));",
     "opacity = abs(opacity);",
     "opacity = 1.0 - pow(opacity, EdgeFalloff);",
      
-    "gl_FragColor = opacity * gl_Color;",
+    "gl_FragColor = opacity * color;",
 "}"
 	].join("\n")
 };
