fa113eb29f14843bf87d469956bc62f728106e5b
fejes6 <fejesandrej@gmail.com>
2016-05-14 16:57:24 +0200

    Update IllustrativeRenderer.js

--- IllustrativeRenderer.js
+++ IllustrativeRenderer.js
@@ -384,11 +384,47 @@ void main() {\
 var fsqFragment = 
 'varying vec2 vUv;\
 uniform sampler2D tDiffuse;\
-uniform float aa;\
+uniform sampler2D hatch0;\
+uniform sampler2D hatch1;\
+uniform sampler2D hatch2;\
+\
+    float shade(const in float shading, const in vec2 uv) {\
+      float shadingFactor;\
+      float stepSize = 1.0 / 3.0;\
+      float alpha = 0.0;\
+      float scaleWhite = 0.0;\
+      float scaleHatch0 = 0.0;\
+      float scaleHatch1 = 0.0;\
+      float scaleHatch2 = 0.0;\
+      if (shading <= stepSize) {\
+        alpha = 3.0 * shading;\
+        scaleHatch1 = alpha;\
+        scaleHatch2 = 1.0 - alpha;\
+      }\
+      else if (shading > stepSize && shading <= 2.0 * stepSize) {\
+        alpha = 3.0 * (shading - stepSize);\
+        scaleHatch0 = alpha;\
+        scaleHatch1 = 1.0 - alpha;\
+      }\
+      else if (shading > 2.0 * stepSize) {\
+        alpha = 3.0 * (shading - stepSize * 2.0);\
+        scaleWhite = alpha;\
+        scaleHatch0 = 1.0 - alpha;\
+      }\
+      shadingFactor = scaleWhite + \
+        scaleHatch0 * texture2D(hatch0, uv).r +\
+        scaleHatch1 * texture2D(hatch1, uv).r +\
+        scaleHatch2 * texture2D(hatch2, uv).r;\
+      return shadingFactor;\
+  }\
 \
 void main() {\
 \
-    gl_FragColor = texture2D( tDiffuse, vUv ) * aa;\
+        vec2 uv = vUv * 15.0;\
+    vec2 uv2 = vUv.yx * 10.0;\
+    float shading = texture2D(tDiffuse, vUv).r + 0.1;\
+    float crossedShading = shade(shading, uv) * shade(shading, uv2) * 0.6 + 0.4;\
+    gl_FragColor = vec4(vec3(crossedShading), 1.0);\
 }';
 
 function IllustrativeRenderer(domQuery) { //for a whole window call with domQuery "<body>"
@@ -431,7 +467,9 @@ function IllustrativeRenderer(domQuery) { //for a whole window call with domQuer
         
         this.fullScreenQuadMaterial = new THREE.ShaderMaterial({
             uniforms: { tDiffuse: { type: "t", value: this.basicRTT },
-            		aa: { type: "f", value: 0.5 }
+            		"hatch0": { type: "t", value: THREE.ImageUtils.loadTexture( "https://rawgit.com/fejes6/RenderViews/master/hatch_0.jpg" ) },
+        		"hatch1": { type: "t", value: THREE.ImageUtils.loadTexture( "https://rawgit.com/fejes6/RenderViews/master/hatch_1.jpg" ) },
+        		"hatch2": { type: "t", value: THREE.ImageUtils.loadTexture( "https://rawgit.com/fejes6/RenderViews/master/hatch_2.jpg" ) }
             },
             vertexShader: fsqVertex,
             fragmentShader: fsqFragment,
