32e7d7f655da2ef1fa23d0354a3e69f9358fb180
fejes6 <fejesandrej@gmail.com>
2016-05-15 01:34:49 +0200

    Update Andrej_initialize.js

--- Andrej/Andrej_initialize.js
+++ Andrej/Andrej_initialize.js
@@ -1,1 +1,5 @@
 RenderWidgets.Add3DRenderer(new IllustrativeRenderer('#basicRendererContainer'));
+
+$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 140px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
+'<div><input type="checkbox" id="effect" name="effects" value="Depth of field"><label for="effect">Depth of field</label></div'+
+'</div>');
