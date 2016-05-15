8ca69f3aff6185c8c4559db673d84acce5063a94
fejes6 <fejesandrej@gmail.com>
2016-05-15 11:43:14 +0200

    Update TestHatchShader.js

--- TestHatchShader.js
+++ TestHatchShader.js
@@ -180,7 +180,7 @@ THREE.TestHatchShader = {
         "}",
     "}",
      
-    "if (lum < 0.3) {",
+    "if (lum < 0.45) {",
         "if (mod(gl_FragCoord.x - gl_FragCoord.y - 5.0, 10.0) == 0.0) {",
             "gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);",
         "}",
