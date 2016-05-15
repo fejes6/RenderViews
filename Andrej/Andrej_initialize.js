bc6e82698a1bf127e4903c469ad76fb4bf10c596
fejes6 <fejesandrej@gmail.com>
2016-05-15 15:19:07 +0200

    Update Andrej_initialize.js

--- Andrej/Andrej_initialize.js
+++ Andrej/Andrej_initialize.js
@@ -1,9 +1,7 @@
 RenderWidgets.Add3DRenderer(new IllustrativeRenderer('#basicRendererContainer'));
 
 $('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 140px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
-'<div><input type="checkbox" id="effect" name="effects" value="Effect"><label for="effect">Sobel filter</label></div'+
+'<div><input type="checkbox" id="effect" name="effects" value="Effect"><label for="effect">Sobel filter    </label></div'+
 '<div><input type="checkbox" id="effect2" name="effects2" value="Effect"><label for="effect">Frei-Chen filter</label></div'+
 '<div><input type="checkbox" id="effect3" name="effects3" value="Effect"><label for="effect">Edge Emphasizing</label></div'+
-'<div><input type="checkbox" id="effect4" name="effects4" value="Effect"><label for="effect">Simple Hatching</label></div'+
-'<div><input type="checkbox" id="effect5" name="effects5" value="Effect"><label for="effect">Test</label></div'+
 '</div>');
