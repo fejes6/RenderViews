7c12b90d167a94fe107d9925f1fc19ae1b78e962
fejes6 <fejesandrej@gmail.com>
2016-05-15 16:52:06 +0200

    Update Andrej_initialize.js

--- Andrej/Andrej_initialize.js
+++ Andrej/Andrej_initialize.js
@@ -1,7 +1,7 @@
 RenderWidgets.Add3DRenderer(new IllustrativeRenderer('#basicRendererContainer'));
 
-$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 180px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
+$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 200px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
 '<div><input type="checkbox" id="effect4" name="effects4" value="Effect"><label for="effect">Simple cross-hatching</label></div'+
-'<div><input type="checkbox" id="effect2" name="effects2" value="Effect"><label for="effect">Frei-Chenov filter</label></div'+
+'<div><input type="checkbox" id="effect2" name="effects2" value="Effect"><label for="effect"><span style="padding-right: 2em">Frei-Chenov filter</span></label></div'+
 '<div><input type="checkbox" id="effect" name="effects" value="Effect"><label for="effect">Sobelov filter</label></div'+
 '</div>');
