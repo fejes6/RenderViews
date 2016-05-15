93538f95111163a2e33a9fcfc90915c5bb5451a8
fejes6 <fejesandrej@gmail.com>
2016-05-15 15:10:58 +0200

    Update Andrej_initialize.js

--- Andrej/Andrej_initialize.js
+++ Andrej/Andrej_initialize.js
@@ -1,6 +1,6 @@
 RenderWidgets.Add3DRenderer(new IllustrativeRenderer('#basicRendererContainer'));
 
-$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 250px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
+$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 1400px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
 '<div><input type="checkbox" id="effect" name="effects" value="Effect"><label for="effect">Sobel filter</label></div'+
 '<div><input type="checkbox" id="effect2" name="effects2" value="Effect"><label for="effect">Frei-Chen filter</label></div'+
 '<div><input type="checkbox" id="effect3" name="effects3" value="Effect"><label for="effect">Edge Emphasizing</label></div'+
