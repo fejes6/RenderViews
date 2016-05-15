df216cc9682b74b0720f0689a1eeaa7117d849f1
fejes6 <fejesandrej@gmail.com>
2016-05-15 15:53:37 +0200

    Update Andrej_initialize.js

--- Andrej/Andrej_initialize.js
+++ Andrej/Andrej_initialize.js
@@ -1,6 +1,6 @@
 RenderWidgets.Add3DRenderer(new IllustrativeRenderer('#basicRendererContainer'));
 
-$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 140px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
+$('#grammar-group').append('<div class="motion_box" style="position: fixed;right: 0px;top: 50px;width: 150px;height: auto;background-color: #f5f5f5;border: 1px solid #ccc;padding: 10px;">'+
 '<div><input type="checkbox" id="effect" name="effects" value="Effect"><label for="effect"><span style="padding-right: 2em">Sobel filter</span></label></div'+
 '<div><input type="checkbox" id="effect2" name="effects2" value="Effect"><label for="effect">Frei-Chen filter</label></div'+
 '<div><input type="checkbox" id="effect4" name="effects4" value="Effect"><label for="effect">Simple Hatching</label></div'+
